e6cacdc3709131d4e22aeb7209b150f322b28f6c
feliciano garcia <30783502+FelicianoGarcia@users.noreply.github.com>
2017-08-16 14:25:31 -0500

    Add files via upload

---
+++ MATEX.java
@@ -0,0 +1,20 @@
+package matex;
+
+/**
+ *
+ * @author chanix
+ */
+public class MATEX {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        formi control = new formi();
+        control.setTitle("PreVi tec");
+        control.setVisible(true);
+        
+    }
+    
+}
